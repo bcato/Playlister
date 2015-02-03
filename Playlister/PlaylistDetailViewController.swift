576c08f530f7b0cc5b1764e6ba253b452a30c4d3
Brandon Cato <brandoncato@airbears2-10-142-54-32.AirBears2.1918.Berkeley.EDU>
2015-02-03 14:40:58 -0800

    adding some untracked files

---
+++ Playlister/PlaylistDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PlaylistDetailViewController.swift
+//  Playlister
+//
+//  Created by Brandon Cato on 2/2/15.
+//  Copyright (c) 2015 BrandonCato. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+    
+    @IBOutlet weak var buttonPressLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Playlister/PlaylistMasterViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Playlister
+//
+//  Created by Brandon Cato on 2/2/15.
+//  Copyright (c) 2015 BrandonCato. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistMasterViewController: UIViewController {
+
+    @IBOutlet weak var aButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+            aButton.setTitle("Press Me", forState: .Normal)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    override func prepareForSegue(segue: UIStoryBoardSegue!, sender: AnyObject?) {
+        if (segue.identifier == "showPlaylistDetail") {
+            let playlistDetailController = segue.destinationViewController as playlistDetailViewController
+        }
+    }
+    
+    
+    
+
+}
+
